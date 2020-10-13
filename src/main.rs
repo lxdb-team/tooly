8845117a3548a10423c967ef1c7ea16c5b361e9f
Denys Konovalov <service@lxdb.de>
2020-10-13 21:47:42 +0200

    Update + { info, lxdb }

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 
 use serenity::{
-    model::{channel::Message, gateway::Ready},
+    model::{channel::Message, gateway::Ready, guild::Emoji},
     prelude::*,
     utils::MessageBuilder,
 };
@@ -20,16 +20,50 @@ impl EventHandler for Handler {
                 },
             };
 
-            // The message builder allows for creating a message by
-            // mentioning users dynamically, pushing "safe" versions of
-            // content (such as bolding normalized content), displaying
-            // emojis, and more.
             let response = MessageBuilder::new()
                 .push("User ")
                 .push_bold_safe(&msg.author.name)
                 .push(" used the 'ping' command in the ")
                 .mention(&channel)
                 .push(" channel")
+                .build();
+
+            if let Err(why) = msg.channel_id.say(&context.http, &response) {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+
+        if msg.content == "!info" {
+            let channel = match msg.channel_id.to_channel(&context) {
+                Ok(channel) => channel,
+                Err(why) => {
+                    println!("Error getting channel: {:?}", why);
+
+                    return;
+                },
+            };
+
+            let response = MessageBuilder::new()
+                .push_bold_safe("What is Tooly ?\n")
+                .push("Tooly is a Discord Bot written in Rust that is going to provide useful tools for server members.")
+                .build();
+
+            if let Err(why) = msg.channel_id.say(&context.http, &response) {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+        if msg.content == "!lxdb" {
+            let channel = match msg.channel_id.to_channel(&context) {
+                Ok(channel) => channel,
+                Err(why) => {
+                    println!("Error getting channel: {:?}", why);
+
+                    return;
+                },
+            };
+
+            let response = MessageBuilder::new()
+                .push("Coming soon")
                 .build();
 
             if let Err(why) = msg.channel_id.say(&context.http, &response) {
