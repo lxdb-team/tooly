cd5ae51b4800ef4be86f36dc690a60d89632d6ef
Denys Konovalov <service@lxdb.de>
2020-10-13 21:27:26 +0200

    Corrected Version Issue

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,29 @@
+use std::env;
+
 use serenity::{
-    async_trait,
     model::{channel::Message, gateway::Ready},
     prelude::*,
-    utils::{EmbedMessageBuilding, MessageBuilder},
+    utils::MessageBuilder,
 };
-use std::env;
 
 struct Handler;
 
-#[async_trait]
 impl EventHandler for Handler {
-    async fn message(&self, context: Context, msg: Message) {
+    fn message(&self, context: Context, msg: Message) {
         if msg.content == "!ping" {
-            let channel = match msg.channel_id.to_channel(&context).await {
+            let channel = match msg.channel_id.to_channel(&context) {
                 Ok(channel) => channel,
                 Err(why) => {
                     println!("Error getting channel: {:?}", why);
+
                     return;
-                }
+                },
             };
+
+            // The message builder allows for creating a message by
+            // mentioning users dynamically, pushing "safe" versions of
+            // content (such as bolding normalized content), displaying
+            // emojis, and more.
             let response = MessageBuilder::new()
                 .push("User ")
                 .push_bold_safe(&msg.author.name)
@@ -27,46 +32,24 @@ impl EventHandler for Handler {
                 .push(" channel")
                 .build();
 
-            if let Err(why) = msg.channel_id.say(&context.http, &response).await {
-                println!("Error sending message: {:?}", why);
-            }
-        }
-
-        if msg.content == "!duck" {
-            let channel = match msg.channel_id.to_channel(&context).await {
-                Ok(channel) => channel,
-                Err(why) => {
-                    println!("Error getting channel: {:?}", why);
-                    return;
-                }
-            };
-            let response = MessageBuilder::new()
-                .push("Hi ")
-                .push_bold_safe(&msg.author.name)
-                .push(", use https://duckduckgo.com")
-                .build();
-
-            if let Err(why) = msg.channel_id.say(&context.http, &response).await {
+            if let Err(why) = msg.channel_id.say(&context.http, &response) {
                 println!("Error sending message: {:?}", why);
             }
         }
     }
 
-    async fn ready(&self, _: Context, ready: Ready) {
+    fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
     }
 }
 
-#[tokio::main]
-async fn main() {
+fn main() {
     // Configure the client with your Discord bot token in the environment.
-    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
-    let mut client = Client::builder(&token)
-        .event_handler(Handler)
-        .await
-        .expect("Err creating client");
+    let token = env::var("DISCORD_TOKEN")
+        .expect("Expected a token in the environment");
+    let mut client = Client::new(&token, Handler).expect("Err creating client");
 
-    if let Err(why) = client.start().await {
+    if let Err(why) = client.start() {
         println!("Client error: {:?}", why);
     }
 }
