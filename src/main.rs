a640c831b171cf66ca1f2941f16e66f35cc4f0ab
Denys Konovalov <service@lxdb.de>
2020-10-14 16:23:38 +0200

    + DDG

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use std::env;
+use std::{env, path::Path};
 
 use serenity::{
     model::{channel::Message, gateway::Ready, guild::Emoji},
     prelude::*,
     utils::MessageBuilder,
+    http::AttachmentType
 };
 
 struct Handler;
@@ -67,6 +68,33 @@ impl EventHandler for Handler {
                 .build();
 
             if let Err(why) = msg.channel_id.say(&context.http, &response) {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+        if msg.content == "!duck" {
+            // The create message builder allows you to easily create embeds and messages
+            // using a builder syntax.
+            // This example will create a message that says "Hello, World!", with an embed that has
+            // a title, description, three fields, and a footer.
+            let msg = msg.channel_id.send_message(&context.http, |m| {
+                m.content("Hello **".to_owned() + &msg.author.name + "**");
+                m.embed(|e| {
+                    e.title("Use DuckDuckGo");
+                    e.description("DuckDuckGo is an internet search engine that emphasizes protecting searcher's privacy and avoiding the filter bubble of personalized search results.");
+                    e.image("attachment://ddg.png");
+                    e.footer(|f| {
+                        f.text("https://en.wikipedia.org/wiki/DuckDuckGo");
+
+                        f
+                    });
+                    e.url("https://duckduckgo.com");
+                    e
+                });
+                m.add_file(AttachmentType::Path(Path::new("./ddg.png")));
+                m
+            });
+
+            if let Err(why) = msg {
                 println!("Error sending message: {:?}", why);
             }
         }
